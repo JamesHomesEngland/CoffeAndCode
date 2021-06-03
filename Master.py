9decc8f2972c2d871cfb30ddfbb2bfcb9e68df03
jamesmh95 <61280487+jamesmh95@users.noreply.github.com>
2021-06-03 09:35:32 +0100

    Update Master.py

--- Master.py
+++ Master.py
@@ -1,32 +1,34 @@
 import pandas as pd
 
-df = pd.read_csv(r"C:\Users\James.Hinkley\OneDrive - Homes England\Documents\Projects\Geoadressing\CoffeAndCode\LR_to_PQI.csv")
+df = pd.read_csv(r"C:\Users\James.Hinkley\OneDrive - Homes England\Documents\Projects\Geoadressing\CoffeAndCode\CofAndCode.csv")
 uprnAdd = df
 for index, row in df.iterrows():
-    actAdds = [row.iloc[0]["building_name"],
-               row.iloc[0]["sub_building_name"],
-               row.iloc[0]["building_number"],
-               row.iloc[0]["thoroughfare"],
-               row.iloc[0]["dep_locality"],
-               row.iloc[0]["postcode"]]
-    print(actAdds)
+    actAdds = [row["building_name"],
+               row["sub_building_name"],
+               row["building_number"],
+               row["thoroughfare"],
+               row["dep_locality"],
+               row["postcode"]]
+
     #actString2 = makeString(actAdds2)
-    # actAdds2 = [row.iloc[0]["sub_building_name"],
-    #             row.iloc[0]["building_name"],
-    #             row.iloc[0]["building_number"],
-    #             row.iloc[0]["thoroughfare"],
-    #             row.iloc[0]["dep_locality"],
-    #             row.iloc[0]["postcode"]]
-    # #actString2 = makeString(actAdds2)
-    # actAdds3 = [row.iloc[0]["sub_building_name"],
-    #             row.iloc[0]["building_name"],
-    #             row.iloc[0]["thoroughfare"],
-    #             row.iloc[0]["dep_locality"],
-    #             row.iloc[0]["postcode"]]
-    # #actString3 = makeString(actAdds3)
-    # actAdds4 = [row.iloc[0]["sub_building_name"],
-    #             row.iloc[0]["building_name"],
-    #             row.iloc[0]["building_number"],
-    #             row.iloc[0]["thoroughfare"],
-    #             row.iloc[0]["postcode"]]
-    # #actString4 = makeString(actAdds4)
+    actAdds2 = [row["sub_building_name"],
+                row["building_name"],
+                row["building_number"],
+                row["thoroughfare"],
+                row["dep_locality"],
+                row["postcode"]]
+    #actString2 = makeString(actAdds2)
+    actAdds3 = [row["sub_building_name"],
+                row["building_name"],
+                row["thoroughfare"],
+                row["dep_locality"],
+                row["postcode"]]
+    #actString3 = makeString(actAdds3)
+    actAdds4 = [row["sub_building_name"],
+                row["building_name"],
+                row["building_number"],
+                row["thoroughfare"],
+                row["postcode"]]
+    #actString4 = makeString(actAdds4)
+    print(actAdds, "\n", actAdds2, "\n", actAdds3, "\n", actAdds4)
+    exit()
